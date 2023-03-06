ce54157b9202b799e0f44683e71a42f7a1e2c867
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 19:29:08 -0300

    Add project files.

---
+++ MediaDeDoisValores/Program.cs
@@ -0,0 +1,10 @@
+﻿float n1, n2;
+
+Console.Write("Digite o valor para  N1: ");
+n1 = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o valor para  N2: ");
+n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("A média é " + ((n1 + n2) / 2).ToString("F1") + "!");
+
